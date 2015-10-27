41691c5fd5b9d0fdf68188eaafb7a7c614f34fab
theapi <petermclarke@gmail.com>
2015-10-27 18:36:10 +0000

    Fixed quadrature_decode using case

--- rotary_encoder/quadrature_decode.v
+++ rotary_encoder/quadrature_decode.v
@@ -18,45 +18,49 @@ module quadrature_decode(
     reg [7:0] total;
     reg [2:0] num;
 
-    always @ (reset)
+    always @ ( * )
     begin
-        combined = 4'b0000;
-        total = 0;
+        if (reset) begin
+            combined = 4'b0000;
+            total = 0;
+            num = 0;
+        end
+        else begin
+            // combine current with previous
+            // left shift 
+            combined = combined << 2;
+            combined = combined | {quadA, quadB};
+
+            num = 0;
+            case (combined)
+                4'b0000 : num =  2'd0;
+                4'b0001 : num = -2'd1;
+                4'b0010 : num =  2'd1;
+                4'b0011 : num =  2'd0;
+
+                4'b0100 : num =  2'd1;
+                4'b0101 : num =  2'd0;
+                4'b0110 : num =  2'd0;
+                4'b0111 : num = -2'd1;
+
+                4'b1000 : num = -2'd1;
+                4'b1001 : num =  2'd0;
+                4'b1010 : num =  2'd0;
+                4'b1011 : num =  2'd1;
+
+                4'b1100 : num =  2'd0;
+                4'b1101 : num =  2'd1;
+                4'b1110 : num = -2'd1;
+                4'b1111 : num =  2'd0;
+            endcase
+            
+            
+            total = $signed(total) + $signed(num);
+            
+            
+        end
     end
-
-    always @ (quadA or quadB)
-    begin
-        // combine current with previous
-        // left shift 
-        combined = combined << 2;
-        combined = combined | {quadA, quadB};
-        
-        case (combined)
-            4'b0000 : num = 2'd0;
-            4'b0001 : num = -2'd1;
-            4'b0010 : num = 2'd1;
-            4'b0011 : num = 2'd0;
-
-            4'b0100 : num = 2'd1;
-            4'b0101 : num = 2'd0;
-            4'b0110 : num = 2'd0;
-            4'b0111 : num = -2'd1;
-
-            4'b1000 : num = -2'd1;
-            4'b1001 : num = 2'd0;
-            4'b1010 : num = 2'd0;
-            4'b1011 : num = 2'd1;
-
-            4'b1100 : num = 2'd0;
-            4'b1101 : num = 2'd1;
-            4'b1110 : num = -2'd1;
-            4'b1111 : num = 2'd0;
-        endcase
-        
-        total = total + num;
-        //GRRR this does not work: count = total;
-    end
-
- 
     
+    assign count = total;
 endmodule
+    
