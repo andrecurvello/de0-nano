4e25ccc3d7609a851a2e068656d0a22db12e696e
theapi <petermclarke@gmail.com>
2015-11-20 13:46:02 +0000

    2 Player scores

--- pong/pong_wave.do
+++ pong/pong_wave.do
@@ -18,9 +18,21 @@ add wave -noupdate -radix decimal /pong_export_tb/DUT/b2v_inst/ball_v
 add wave -noupdate /pong_export_tb/DUT/b2v_inst/ball_v_direction
 add wave -noupdate -radix decimal /pong_export_tb/DUT/b2v_inst/paddle_a_pos
 add wave -noupdate -radix decimal /pong_export_tb/DUT/b2v_inst/paddle_b_pos
+add wave -noupdate /pong_export_tb/DUT/b2v_inst/paddle_a
+add wave -noupdate /pong_export_tb/DUT/b2v_inst/paddle_b
+add wave -noupdate /pong_export_tb/DUT/b2v_inst/net
+add wave -noupdate /pong_export_tb/DUT/b2v_inst/border
+add wave -noupdate -divider Score
+add wave -noupdate -radix decimal /pong_export_tb/DUT/b2v_inst/score_1/PLAYER_ONE
+add wave -noupdate -radix decimal /pong_export_tb/DUT/b2v_inst/score_1/PLAYER_TWO
+add wave -noupdate -radix hexadecimal /pong_export_tb/DUT/b2v_inst/score_1/rom_addr
+add wave -noupdate -radix hexadecimal /pong_export_tb/DUT/b2v_inst/score_1/char_addr
+add wave -noupdate -radix hexadecimal /pong_export_tb/DUT/b2v_inst/score_1/bit_addr
+add wave -noupdate /pong_export_tb/DUT/b2v_inst/score_1/p1
+add wave -noupdate /pong_export_tb/DUT/b2v_inst/score_1/p2
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {0 ns} 0}
-quietly wave cursor active 0
+WaveRestoreCursors {{Cursor 1} {401650 ns} 0}
+quietly wave cursor active 1
 configure wave -namecolwidth 150
 configure wave -valuecolwidth 100
 configure wave -justifyvalue left
@@ -35,4 +47,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ns
 update
-WaveRestoreZoom {20050 us} {41050 us}
+WaveRestoreZoom {0 ns} {10500 us}
--- pong/score.v
+++ pong/score.v
@@ -13,32 +13,49 @@ module score (
     reg [2:0] bit_addr;
     wire [7:0] font_word;
     wire font_bit;
-    wire text_on;
+    wire p1, p2;
    
 
     // instantiate font ROM
     font_rom font_unit
     (.clk(clk), .addr(rom_addr), .data(font_word));
       
-    // Location on the screen of this tile.
-    reg [10:0] tile_v_start = 11'd10;
-    reg [10:0] tile_h_start = 11'd280;
-    wire [10:0] tile_v_end;
-    wire [10:0] tile_h_end;
-    assign tile_v_end = tile_v_start + (8'd15 << 2);
-    assign tile_h_end = tile_h_start + (8'd7 << 2);
-    // Check we are within the part of the screen that holds the character tile.
-    assign text_on = (PIXEL_H >= tile_h_start && PIXEL_H <= tile_h_end && PIXEL_V >= tile_v_start && PIXEL_V <= tile_v_end);
+    // Location on the screen of the player one score.
+    reg [10:0] p1_v_start = 11'd10;
+    reg [10:0] p1_h_start = 11'd280;
+    
+    // Location on the screen of the player two score.
+    reg [10:0] p2_v_start = 11'd10;
+    reg [10:0] p2_h_start = 11'd380;
+
+    // Check we are within the part of the screen that holds the player one score.
+    assign p1 = (
+        PIXEL_H >= p1_h_start && PIXEL_H <= p1_h_start + (8'd7 << 2) 
+        && PIXEL_V >= p1_v_start && PIXEL_V <= p1_v_start + (8'd15 << 2)
+    );
+    
+    // Check we are within the part of the screen that holds the player two score.
+    assign p2 = (
+        PIXEL_H >= p2_h_start && PIXEL_H <= p2_h_start + (8'd7 << 2) 
+        && PIXEL_V >= p2_v_start && PIXEL_V <= p2_v_start + (8'd15 << 2)
+    );
  
 
     // Mux for font ROM addresses and rgb
     always @* begin
         PIXEL = 3'b000;
-        if (text_on) begin
+        if (p1) begin
+        // @todo Scores larger than 9!!!
+            char_addr = 7'h30 + PLAYER_ONE;
+            row_addr = (PIXEL_V - p1_v_start) >> 2;
+            bit_addr = (PIXEL_H - p1_h_start) >> 2;
+            if (font_bit)
+               PIXEL = 3'b111;
+        end else if (p2) begin
         // @todo Scores larger than 9!!!
             char_addr = 7'h30 + PLAYER_TWO;
-            row_addr = (PIXEL_V - tile_v_start) >> 2;
-            bit_addr = (PIXEL_H - tile_h_start) >> 2;
+            row_addr = (PIXEL_V - p2_v_start) >> 2;
+            bit_addr = (PIXEL_H - p2_h_start) >> 2;
             if (font_bit)
                PIXEL = 3'b111;
         end else begin
