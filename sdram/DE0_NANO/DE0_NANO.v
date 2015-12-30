7372c1827263b5386d660c0b250958d9ef3759c4
theapi <petermclarke@gmail.com>
2015-12-30 16:15:38 +0000

    Read & write in seperate always blocks

--- sdram/DE0_NANO/DE0_NANO.v
+++ sdram/DE0_NANO/DE0_NANO.v
@@ -106,21 +106,21 @@ reg [15:0] r_q = 16'b0;
 
 
 qsys u0(
-		.clk_clk(CLOCK_50),								//clk.clk
-		.reset_reset_n(1'b1),							//reset.reset_n
+		.clk_clk(CLOCK_50),							//clk.clk
+		.reset_reset_n(1'b1),						//reset.reset_n
 		
 		.sdram_clock_c0_clk(DRAM_CLK),				//sdram_clock_c0.clk
 		.sdram_clock_areset_conduit_export(1'b0),	//sdram_clock_areset_conduit.export
 		
-		.sdram_wire_addr(DRAM_ADDR),					//sdram_wire.addr
-		.sdram_wire_ba(DRAM_BA),						//           .ba
+		.sdram_wire_addr(DRAM_ADDR),				//sdram_wire.addr
+		.sdram_wire_ba(DRAM_BA),					//           .ba
 		.sdram_wire_cas_n(DRAM_CAS_N),				//           .cas_n
-		.sdram_wire_cke(DRAM_CKE),						//           .cke
-		.sdram_wire_cs_n(DRAM_CS_N),					//           .cs_n
-		.sdram_wire_dq(DRAM_DQ),						//           .dq
-		.sdram_wire_dqm(DRAM_DQM),						//           .dqm
+		.sdram_wire_cke(DRAM_CKE),					//           .cke
+		.sdram_wire_cs_n(DRAM_CS_N),				//           .cs_n
+		.sdram_wire_dq(DRAM_DQ),					//           .dq
+		.sdram_wire_dqm(DRAM_DQM),					//           .dqm
 		.sdram_wire_ras_n(DRAM_RAS_N),				//           .ras_n
-		.sdram_wire_we_n(DRAM_WE_N),					//           .we_n
+		.sdram_wire_we_n(DRAM_WE_N),				//           .we_n
 		
 		.sdram_read_control_fixed_location  (1'b1), // do not auto increment address
 		.sdram_read_control_read_base       (read_base),
@@ -132,14 +132,14 @@ qsys u0(
 		.sdram_read_user_buffer_output_data (output_data),
 		.sdram_read_user_data_available     (read_data_available),
         
-        .sdram_write_user_write_buffer      (write_buffer),      //           sdram_write_user.write_buffer
-		.sdram_write_user_buffer_input_data (input_data), //                           .buffer_input_data
-		.sdram_write_user_buffer_full       (write_buffer_full),       //                           .buffer_full
+        .sdram_write_user_write_buffer      (write_buffer),
+		.sdram_write_user_buffer_input_data (input_data),
+		.sdram_write_user_buffer_full       (write_buffer_full),
 		.sdram_write_control_fixed_location (1'b1), // do not auto increment address
-		.sdram_write_control_write_base     (write_base),     //                           .write_base
+		.sdram_write_control_write_base     (write_base),
 		.sdram_write_control_write_length   (32'd2),  // 16 bit word
-		.sdram_write_control_go             (write_go),             //                           .go
-		.sdram_write_control_done           (write_done)  
+		.sdram_write_control_go             (write_go),
+		.sdram_write_control_done           (write_done)
 );
 
 assign read_base    = r_read_address;
@@ -151,7 +151,7 @@ assign input_data   = r_input_data;
 assign write_base   = r_write_address;
 assign write_go     = r_write_en;
 
-assign LED = r_q[15:9];
+assign LED = r_q[7:0];
 
     always @(posedge CLOCK_50) begin
         if (counter == 32'd25000000) begin
@@ -159,6 +159,37 @@ assign LED = r_q[15:9];
             // stop
         end else begin
             counter <= counter + 32'b1;
+        end
+    end
+        
+    // Write
+    always @(posedge CLOCK_50) begin
+        if (counter == 32'd2) begin
+            // Write data.
+            r_write_address <= 32'd0;
+            
+            //r_input_data <= 16'b1100_1100_1100_1100;
+            //r_input_data <= 16'b1010_1010_1010_1010;
+            r_input_data <= 16'b1111_0000_1110_1101;
+            r_write_buffer <= 1'b1;
+            r_write_en <= 1'b1;
+        end else begin
+            // Ensure the request only lasts one clock cycle.
+            r_write_buffer <= 1'b0;
+            r_write_en <= 1'b0;
+        end
+        
+    end
+
+    // Read
+    always @(posedge CLOCK_50) begin
+        if (counter == 32'd3) begin
+            // Send the read command.
+            r_read_address <= 32'd0;
+            r_read_en <= 1'b1;
+        end else begin
+            // Ensure the request only lasts one clock cycle.
+            r_read_en <= 1'b0;
         end
     end
     
@@ -169,30 +200,6 @@ assign LED = r_q[15:9];
         end else begin
             r_read_buffer <= 1'b0;
         end
-    end
-
-    always @(posedge CLOCK_50) begin
-        if (counter == 32'd2) begin
-            // Write data.
-            r_write_address <= 32'd0;
-            r_write_en <= 1'b1;
-            //r_input_data <= 16'b1100_1100_1100_1100;
-            //r_input_data <= 16'b1010_1010_1010_1010;
-            r_input_data <= 16'b1111_0000_1110_1101;
-            r_write_buffer <= 1'b1;
-        end else if (counter == 32'd3) begin
-            // Ensure the request only lasts one clock cycle.
-            r_write_buffer <= 1'b0;
-            r_write_en <= 1'b0;
-        
-            // Send the read command.
-            r_read_address <= 0;
-            r_read_en <= 1;
-        end if (r_read_en) begin
-            // Ensure the request only lasts one clock cycle.
-            r_read_en <= 0;
-        end
-        
     end
 
 endmodule
