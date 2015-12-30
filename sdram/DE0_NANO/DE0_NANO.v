490b2793535513b5923b1712b17fbdf817d93062
theapi <petermclarke@gmail.com>
2015-12-30 16:22:27 +0000

    Formating

--- sdram/DE0_NANO/DE0_NANO.v
+++ sdram/DE0_NANO/DE0_NANO.v
@@ -5,29 +5,29 @@
 
 module DE0_NANO(
 
-	//////////// CLOCK //////////
-	CLOCK_50,
+    //////////// CLOCK //////////
+    CLOCK_50,
 
-	//////////// LED //////////
-	LED,
+    //////////// LED //////////
+    LED,
 
-	//////////// KEY //////////
-	KEY,
+    //////////// KEY //////////
+    KEY,
 
-	//////////// SW //////////
-	SW,
+    //////////// SW //////////
+    SW,
 
-	//////////// SDRAM //////////
-	DRAM_ADDR,
-	DRAM_BA,
-	DRAM_CAS_N,
-	DRAM_CKE,
-	DRAM_CLK,
-	DRAM_CS_N,
-	DRAM_DQ,
-	DRAM_DQM,
-	DRAM_RAS_N,
-	DRAM_WE_N 
+    //////////// SDRAM //////////
+    DRAM_ADDR,
+    DRAM_BA,
+    DRAM_CAS_N,
+    DRAM_CKE,
+    DRAM_CLK,
+    DRAM_CS_N,
+    DRAM_DQ,
+    DRAM_DQM,
+    DRAM_RAS_N,
+    DRAM_WE_N 
 );
 
 //=======================================================
@@ -40,28 +40,28 @@ module DE0_NANO(
 //=======================================================
 
 //////////// CLOCK //////////
-input 		          		CLOCK_50;
+input               CLOCK_50;
 
 //////////// LED //////////
-output		     [7:0]		LED;
+output       [7:0]  LED;
 
 //////////// KEY //////////
-input 		     [1:0]		KEY;
+input        [1:0]  KEY;
 
 //////////// SW //////////
-input 		     [3:0]		SW;
+input        [3:0]  SW;
 
 //////////// SDRAM //////////
-output		    [12:0]		DRAM_ADDR;
-output		     [1:0]		DRAM_BA;
-output		          		DRAM_CAS_N;
-output		          		DRAM_CKE;
-output		          		DRAM_CLK;
-output		          		DRAM_CS_N;
-inout 		    [15:0]		DRAM_DQ;
-output		     [1:0]		DRAM_DQM;
-output		          		DRAM_RAS_N;
-output		          		DRAM_WE_N;
+output      [12:0]  DRAM_ADDR;
+output       [1:0]  DRAM_BA;
+output              DRAM_CAS_N;
+output              DRAM_CKE;
+output              DRAM_CLK;
+output              DRAM_CS_N;
+inout       [15:0]  DRAM_DQ;
+output       [1:0]  DRAM_DQM;
+output              DRAM_RAS_N;
+output              DRAM_WE_N;
 
 
 //=======================================================
@@ -73,74 +73,74 @@ output		          		DRAM_WE_N;
 
 // Read
 wire [31:0] read_base;
-wire		read_go;
-wire		read_buffer;
-wire		read_done;
-wire		read_early_done;
-wire		read_data_available;
+wire        read_go;
+wire        read_buffer;
+wire        read_done;
+wire        read_early_done;
+wire        read_data_available;
 wire [15:0] output_data;
 
-reg	[31:0]  r_read_address;
-reg		    r_read_en;
-reg		    r_read_buffer;
+reg [31:0]  r_read_address;
+reg         r_read_en;
+reg         r_read_buffer;
 
 // Write
-wire [31:0]	write_base;
+wire [31:0] write_base;
 wire        write_go;
 wire        write_buffer;
-wire		write_buffer_full;
-wire		write_done;
-wire [15:0]	input_data;
+wire        write_buffer_full;
+wire        write_done;
+wire [15:0] input_data;
 
-reg	[31:0]	r_write_address;
-reg		    r_write_en;
-reg		    r_write_buffer;
-reg	[15:0]	r_input_data;
+reg [31:0]  r_write_address;
+reg         r_write_en;
+reg         r_write_buffer;
+reg [15:0]  r_input_data;
 
-reg [31:0] counter = 32'b0;
-reg [15:0] r_q = 16'b0;
+reg [31:0]  counter = 32'b0;
+reg [15:0]  r_q = 16'b0;
 
 //=======================================================
 //  Structural coding
 //=======================================================
 
 
-qsys u0(
-		.clk_clk(CLOCK_50),							//clk.clk
-		.reset_reset_n(1'b1),						//reset.reset_n
-		
-		.sdram_clock_c0_clk(DRAM_CLK),				//sdram_clock_c0.clk
-		.sdram_clock_areset_conduit_export(1'b0),	//sdram_clock_areset_conduit.export
-		
-		.sdram_wire_addr(DRAM_ADDR),				//sdram_wire.addr
-		.sdram_wire_ba(DRAM_BA),					//           .ba
-		.sdram_wire_cas_n(DRAM_CAS_N),				//           .cas_n
-		.sdram_wire_cke(DRAM_CKE),					//           .cke
-		.sdram_wire_cs_n(DRAM_CS_N),				//           .cs_n
-		.sdram_wire_dq(DRAM_DQ),					//           .dq
-		.sdram_wire_dqm(DRAM_DQM),					//           .dqm
-		.sdram_wire_ras_n(DRAM_RAS_N),				//           .ras_n
-		.sdram_wire_we_n(DRAM_WE_N),				//           .we_n
-		
-		.sdram_read_control_fixed_location  (1'b1), // do not auto increment address
-		.sdram_read_control_read_base       (read_base),
-		.sdram_read_control_read_length     (32'd2), // 16 bit word
-		.sdram_read_control_go              (read_go),
-		.sdram_read_control_done            (read_done),
-		.sdram_read_control_early_done      (read_early_done),
-		.sdram_read_user_read_buffer        (read_buffer),
-		.sdram_read_user_buffer_output_data (output_data),
-		.sdram_read_user_data_available     (read_data_available),
-        
+    qsys u0(
+        .clk_clk(CLOCK_50),       //clk.clk
+        .reset_reset_n(1'b1),      //reset.reset_n
+
+        .sdram_clock_c0_clk(DRAM_CLK),    //sdram_clock_c0.clk
+        .sdram_clock_areset_conduit_export(1'b0), //sdram_clock_areset_conduit.export
+
+        .sdram_wire_addr(DRAM_ADDR),    //sdram_wire.addr
+        .sdram_wire_ba(DRAM_BA),     //           .ba
+        .sdram_wire_cas_n(DRAM_CAS_N),    //           .cas_n
+        .sdram_wire_cke(DRAM_CKE),     //           .cke
+        .sdram_wire_cs_n(DRAM_CS_N),    //           .cs_n
+        .sdram_wire_dq(DRAM_DQ),     //           .dq
+        .sdram_wire_dqm(DRAM_DQM),     //           .dqm
+        .sdram_wire_ras_n(DRAM_RAS_N),    //           .ras_n
+        .sdram_wire_we_n(DRAM_WE_N),    //           .we_n
+
+        .sdram_read_control_fixed_location  (1'b1), // do not auto increment address
+        .sdram_read_control_read_base       (read_base),
+        .sdram_read_control_read_length     (32'd2), // 16 bit word
+        .sdram_read_control_go              (read_go),
+        .sdram_read_control_done            (read_done),
+        .sdram_read_control_early_done      (read_early_done),
+        .sdram_read_user_read_buffer        (read_buffer),
+        .sdram_read_user_buffer_output_data (output_data),
+        .sdram_read_user_data_available     (read_data_available),
+
         .sdram_write_user_write_buffer      (write_buffer),
-		.sdram_write_user_buffer_input_data (input_data),
-		.sdram_write_user_buffer_full       (write_buffer_full),
-		.sdram_write_control_fixed_location (1'b1), // do not auto increment address
-		.sdram_write_control_write_base     (write_base),
-		.sdram_write_control_write_length   (32'd2),  // 16 bit word
-		.sdram_write_control_go             (write_go),
-		.sdram_write_control_done           (write_done)
-);
+        .sdram_write_user_buffer_input_data (input_data),
+        .sdram_write_user_buffer_full       (write_buffer_full),
+        .sdram_write_control_fixed_location (1'b1), // do not auto increment address
+        .sdram_write_control_write_base     (write_base),
+        .sdram_write_control_write_length   (32'd2),  // 16 bit word
+        .sdram_write_control_go             (write_go),
+        .sdram_write_control_done           (write_done)
+    );
 
 assign read_base    = r_read_address;
 assign read_go      = r_read_en;
@@ -161,7 +161,7 @@ assign LED = r_q[7:0];
             counter <= counter + 32'b1;
         end
     end
-        
+
     // Write
     always @(posedge CLOCK_50) begin
         if (counter == 32'd2) begin
@@ -192,7 +192,7 @@ assign LED = r_q[7:0];
             r_read_en <= 1'b0;
         end
     end
-    
+
     always @(posedge CLOCK_50) begin
         if (read_data_available) begin
             r_q <= output_data;
